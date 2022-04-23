8e77d96ee5a3929601d5c6206b1168386c228740
Krotesk <igor_98_zukov@mail.ru>
2022-04-23 22:12:49 +0300

    insert added

--- scripts.js
+++ scripts.js
@@ -1,23 +1,26 @@
 class CustomTr extends HTMLElement {
     /** Кастомный элемент для строки с цветами*/
+    constructor() {
+        super();
+    }
     connectedCallback() {
         let table = document.querySelector('#tableBody');
         let template = document.querySelector('#customTemplate')
-        let html = document.importNode(template.content, true);
-        table.appendChild(html);
+
+        let clone = template.content.cloneNode(true);
+        let td = clone.querySelectorAll("td");
+
+        let name=this.getAttribute('name');
+        let type=this.getAttribute('type');
+        let color=this.getAttribute('color');
+        td[1].textContent = name
+        td[2].textContent = type
+        td[3].textContent = color.toUpperCase()
+
+        table.appendChild(clone)
     }
 }
 customElements.define('custom-tr', CustomTr);
-
-const insertRow = ()=>{
-    /** Добавление строки в DOM */
-
-    let table = document.querySelector('#tableBody');
-    let template = document.querySelector('#customTemplate')
-    let html = document.importNode(template.content, true);
-    table.appendChild(html)
-}
-
 
 const save = ()=>{
     /** Сохранение массива объектов в localStorage*/
@@ -36,11 +39,52 @@ const save = ()=>{
     localStorage.setItem('colors',colorPalette);
 }
 
+const getValues = (row) =>{
+    let inputs = row.getElementsByTagName('input'); //nodeList
+    /** Возвращаем объект, состоящий из значений инпутов
+     *  ( в одной строке много, и мы знаем их индексы в nodeList)
+     * Индексы странные из-за особенностей кастомного select */
+    return  {
+        name:inputs.item(0).value,
+        color:inputs.item(7).value,
+    };
+}
 /***** Modal *****/
 
-const addColor = () =>{
-
+const addColor = (e) =>{
+    e.preventDefault()
 }
 const onClose = () =>{
 
 }
+const insertRow = ({color,name,type})=> {
+    /** Добавление кастомного элемента в DOM */
+
+    let table = document.querySelector('#tableBody');
+    let row = document.createElement("custom-tr");
+    row.setAttribute('name',name);
+    row.setAttribute('color',color);
+    row.setAttribute('type',type);
+
+    table.appendChild(row)
+}
+
+modalForm.onsubmit = (e) =>{
+    /** modalForm - это id формы */
+    e.preventDefault();
+
+    let data = new FormData(modalForm);
+    let type = "";
+    for (let entry of data.entries()) {
+        /** .entries() достает единственную пару ключ-значение.
+         * entry[0] - ключ, entry[1] - значение */
+        type =  entry[1] ;
+    };
+    let {name,color} = getValues(e.target)
+    insertRow({
+        name,
+        type,
+        color
+    });
+}
+
